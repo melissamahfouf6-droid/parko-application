9b74af88088d2deb535d6f03fbfea751a37d535b
melissa <abdullah_nyr11@abdullahs-MacBook-Air.local>
2026-05-27 19:47:07 +0100

    fix: unblock CI (intl + widget test)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:parko/app/app.dart';
+import 'package:parko/core/storage/preferences_bootstrap.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
+    await PreferencesBootstrap.init();
+  });
+
   testWidgets('Parko shows brand on splash', (WidgetTester tester) async {
     await tester.pumpWidget(const ProviderScope(child: ParkoApp()));
     await tester.pump(const Duration(milliseconds: 100));
